c42549f91e75eab5cdf4d750cf1b33a7301105a0
Enis Yavuz <enis.1739@gmail.com>
2023-05-18 22:17:23 +0200

    Update archiveTests.m

--- ios/archiveTests/archiveTests.m
+++ ios/archiveTests/archiveTests.m
@@ -1,8 +1,5 @@
 #import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
-
-#import <React/RCTLog.h>
-
+#import <XCTest/XCTest.
 
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
